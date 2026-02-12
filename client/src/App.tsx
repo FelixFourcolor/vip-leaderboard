70dd93cfc92810534881d45ec1c2c0f84c1c32b4
Felix <felix.fourcolor@gmail.com>
2026-02-12 12:49:25 -0800

    remove dependency on styled-components

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,5 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import styled from "styled-components";
+// Removed styled-components usage; using inline styles instead.
 import { Chart } from "@/components/Chart";
 import { Footer } from "@/components/Footer";
 import "./styles.css";
@@ -9,23 +9,23 @@ const queryClient = new QueryClient();
 export default function () {
 	return (
 		<QueryClientProvider client={queryClient}>
-			<Page>
-				<Main>
+			<div
+				style={{
+					minHeight: "100vh",
+					display: "flex",
+					flexDirection: "column",
+				}}
+			>
+				<div
+					style={{
+						flex: "1 0 auto",
+						padding: "1rem",
+					}}
+				>
 					<Chart height={500} />
-				</Main>
+				</div>
 				<Footer />
-			</Page>
+			</div>
 		</QueryClientProvider>
 	);
 }
-
-const Page = styled.div`
-	min-height: 100vh;
-	display: flex;
-	flex-direction: column;
-`;
-
-const Main = styled.div`
-	flex: 1 0 auto;            
-    padding: 1rem;
-`;
