799e6abff7529b0aff01ca524549a496e2a6153b
Felix <felix.fourcolor@gmail.com>
2026-02-19 11:47:41 -0800

    remove tanstack query dep

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,7 @@
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { createRouter, RouterProvider } from "@tanstack/react-router";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { routeTree } from "./routeTree.gen";
-
-const queryClient = new QueryClient();
 
 const router = createRouter({
 	routeTree,
@@ -15,9 +12,7 @@ const router = createRouter({
 
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
-		<QueryClientProvider client={queryClient}>
-			<RouterProvider router={router} />
-		</QueryClientProvider>
+		<RouterProvider router={router} />
 	</StrictMode>,
 );
 
