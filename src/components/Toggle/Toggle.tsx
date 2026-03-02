1b6a007bb28c4d5e5cfadaff42c4db4a16c7ba38
Felix <felix.fourcolor@gmail.com>
2026-03-01 16:16:41 -0800

    simplify toggle

--- src/components/Toggle/Toggle.tsx
+++ src/components/Toggle/Toggle.tsx
@@ -1,12 +1,11 @@
 import classNames from "classnames/bind";
 import type { CSSProperties, ReactNode } from "react";
-import { useControlled } from "@/hooks/useControlled";
 import styles from "./Toggle.module.css";
 
 const cx = classNames.bind(styles);
 
 interface ToggleProps {
-	value?: boolean;
+	value: boolean;
 	onChange: (isOn: boolean) => void;
 	customStyles?: Partial<Record<"container" | "slider", CSSProperties>>;
 	className?: string;
@@ -14,24 +13,19 @@ interface ToggleProps {
 }
 
 export function Toggle({
-	value = false,
+	value: isOn = false,
 	onChange,
 	customStyles = {},
 	children,
 	className,
 	...props
 }: ToggleProps) {
-	const [isOn, setIsOn] = useControlled(value);
-
 	return (
 		<label {...props} className={cx("container", className)}>
 			<input
 				type="checkbox"
 				checked={isOn}
-				onChange={({ target: { checked } }) => {
-					setIsOn(checked);
-					onChange(checked);
-				}}
+				onChange={({ target: { checked } }) => onChange(checked)}
 			/>
 			<div className={cx("toggle", { isOn })} style={customStyles.container}>
 				<div className={cx("dummy")} />
