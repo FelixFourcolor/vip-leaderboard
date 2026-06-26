2dfbf07fd5bce2d1068200cce3fad0793eb43b2a
Felix <felix.fourcolor@gmail.com>
2026-06-26 13:20:05 +0700

    tweak chart wheel rate

--- src/components/TimeChart/layers/Interaction/wheel.ts
+++ src/components/TimeChart/layers/Interaction/wheel.ts
@@ -4,19 +4,26 @@ import { useChartZoom } from "../../zoomContext";
 import { usePanHandler } from "./pan";
 import { useZoomHandler } from "./zoom";
 
+const WHEEL_SPEED = 1 / 16;
+const THROTTLE_MS = 24;
+
 export function useWheel() {
 	const { setIsInteracting } = useChartZoom();
-	const pan = useThrottle(usePanHandler(), 12);
-	const zoom = useThrottle(useZoomHandler(), 12);
+	const pan = useThrottle(usePanHandler(), THROTTLE_MS);
+	const zoom = useThrottle(useZoomHandler(), THROTTLE_MS);
 	const timeoutRef = useRef<number>(undefined);
 
 	return (e: WheelEvent) => {
 		e.preventDefault();
-		if (e.ctrlKey) {
-			const direction = -Math.sign(e.deltaY);
-			zoom(direction / 24, direction / 24);
+		const { ctrlKey, deltaX, deltaY } = e;
+		if (ctrlKey) {
+			const direction = -Math.sign(deltaY);
+			// speed / 2 because the ends move in opposite directions,
+			// effectively doubling the rate
+			const magnitude = (WHEEL_SPEED / 2) * direction;
+			zoom(magnitude, magnitude);
 		} else {
-			pan(Math.sign(e.deltaX) / 16, -Math.sign(e.deltaY) / 16);
+			pan(WHEEL_SPEED * Math.sign(deltaX), WHEEL_SPEED * -Math.sign(deltaY));
 		}
 		clearTimeout(timeoutRef.current);
 		setIsInteracting(true);
